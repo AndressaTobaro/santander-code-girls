b78b5ce0c2f63a4044455fd042bb42f37cf8acaa
Andressa Tobaro <dessamatobaro@gmail.com>
2022-07-18 23:26:05 -0300

    Adicionando Tratamento de Exceções em Java

---
+++ Módulo III/04- Tratamento de Exceções em Java/exception/src/br/com/dio/exceptions/DivisaoNaoExataException.java
@@ -0,0 +1,13 @@
+package br.com.dio.exceptions;
+
+public class DivisaoNaoExataException extends Exception{
+
+    private int numerador;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+}
